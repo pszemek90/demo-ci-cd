a3e4db14a2c1f8dded36e6fcec38d55138d432db
pszemek90 <przemyslaw_miekos@o2.pl>
2021-01-24 15:49:47 +0100

    Initial commit

---
+++ src/test/java/pl/pszemek/CloudCiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pszemek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCiCdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
